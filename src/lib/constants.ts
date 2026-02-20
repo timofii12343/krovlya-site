e8640233a62bd164c86bb2aa58761070d21d7cfa
timofii12343 <timofiiborovykov@gmail.com>
2026-02-20 14:11:38 +0100

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const PHONE = '+7 (980) 011-33-28';
+export const PHONE = '+7 (980) 011-00-28';
 export const PHONE_LINK = 'tel:+79800113328';
 export const TELEGRAM_LINK = 'https://t.me/D_0X_D';
 export const WHATSAPP_LINK = 'https://wa.me/4917631786907';
